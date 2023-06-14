feb38ddd5806325302c23b7197a11f03432ff011
nai-degen <khoners@protonmail.com>
2023-06-13 21:46:21 -0500

    adds hard-coded janitor block

--- src/proxy/check-origin.ts
+++ src/proxy/check-origin.ts
@@ -1,7 +1,11 @@
 import { config } from "../config";
 import { RequestHandler } from "express";
 
-const BLOCKED_REFERERS = config.blockedOrigins?.split(",") || [];
+const TIKTOK_ZOOMER_ORIGIN = "janitorai.com";
+
+const BLOCKED_REFERERS = (config.blockedOrigins?.split(",") || []).concat([
+  TIKTOK_ZOOMER_ORIGIN,
+]);
 
 /** Disallow requests from blocked origins and referers. */
 export const checkOrigin: RequestHandler = (req, res, next) => {
@@ -22,7 +26,7 @@ export const checkOrigin: RequestHandler = (req, res, next) => {
         req.headers["content-type"]?.includes("application/json");
       if (!req.accepts("html") || hasJsonBody) {
         return res.status(403).json({
-          error: { type: "blocked_origin", message: config.blockMessage },
+          error: { type: "blocked_origin", message: getBlockMessage(block) },
         });
       } else {
         const destination = config.blockRedirect || "https://openai.com";
@@ -33,7 +37,7 @@ export const checkOrigin: RequestHandler = (req, res, next) => {
   <meta http-equiv="refresh" content="3; url=${destination}" />
 </head>
 <body style="font-family: sans-serif; height: 100vh; display: flex; flex-direction: column; justify-content: center; text-align: center;">
-<h2>${config.blockMessage}</h3>
+<h2>${getBlockMessage(block)}</h2>
 <p><strong>Please hold while you are redirected to a more suitable service.</strong></p>
 </body>
 </html>`
@@ -43,3 +47,11 @@ export const checkOrigin: RequestHandler = (req, res, next) => {
   }
   next();
 };
+
+function getBlockMessage(origin: string) {
+  if (origin.includes(TIKTOK_ZOOMER_ORIGIN)) {
+    return `This OpenAI account has been disabled due to fraud and abuse. Your IP address, user agent, and request details have been logged and will be shared with law enforcement's cybercrime division to assist in their investigation.`;
+  } else {
+    return config.blockMessage;
+  }
+}
