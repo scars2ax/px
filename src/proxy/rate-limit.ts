28dc271781088ec319155798b38ce55901bb7229
Kwaroran <48973-kwaroran@users.noreply.gitgud.io>
2023-07-21 16:41:51 +0000

    Implement rate limit for risuai.xyz

--- src/proxy/rate-limit.ts
+++ src/proxy/rate-limit.ts
@@ -1,12 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import axios from 'axios'
 import { config } from "../config";
 
 export const AGNAI_DOT_CHAT_IP = "157.230.249.32";
+const RISUAI_TOKEN_CHECKER_URL = "https://sv.risuai.xyz/public/api/checktoken"
 const RATE_LIMIT_ENABLED = Boolean(config.modelRateLimit);
 const RATE_LIMIT = Math.max(1, config.modelRateLimit);
 const ONE_MINUTE_MS = 60 * 1000;
 
 const lastAttempts = new Map<string, number[]>();
+let bitFreshRisuTokens:Array<string> = []
+let freshRisuTokens:Array<string> = []
+let lastRisuTokenTime = 0
 
 const expireOldAttempts = (now: number) => (attempt: number) =>
   attempt > now - ONE_MINUTE_MS;
@@ -52,7 +57,7 @@ export const getUniqueIps = () => {
   return lastAttempts.size;
 };
 
-export const ipLimiter = (req: Request, res: Response, next: NextFunction) => {
+export const ipLimiter = async (req: Request, res: Response, next: NextFunction) => {
   if (!RATE_LIMIT_ENABLED) {
     next();
     return;
@@ -66,9 +71,49 @@ export const ipLimiter = (req: Request, res: Response, next: NextFunction) => {
     return;
   }
 
+  // makes risuai.xyz rate limiting by x-risu-tk header since it's shared between a lot of users.
+  let risuToken:string|null = req.header("x-risu-tk")
+  if (risuToken){
+
+    try{
+      // checks the token only when it is not in freshRisuTokens or bitFreshRisuTokens
+      if(!(freshRisuTokens.includes(risuToken) || bitFreshRisuTokens.includes(risuToken))){
+        // checks the token is vaild (fresh) to prevend abuse
+        const vaildCheck = await axios.post(RISUAI_TOKEN_CHECKER_URL, {
+          token: risuToken
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        if(!vaildCheck.vaild){
+          //if its invaild, uses ip instead
+          risuToken = null
+        }
+      }
+
+      //Cycle fresh status of tokens
+      const minNow = Math.floor(Date.now() / 60000)
+      if(lastRisuTokenTime === 0){
+        lastRisuTokenTime = minNow
+      }
+      if (minNow !== lastRisuTokenTime){
+        bitFreshRisuTokens = freshRisuTokens
+        freshRisuTokens = []
+        lastRisuTokenTime = minNow
+      }
+    }
+    catch{
+      //if request throws error, uses ip
+      risuToken = null
+    }
+  }
+
+
   // If user is authenticated, key rate limiting by their token. Otherwise, key
   // rate limiting by their IP address. Mitigates key sharing.
-  const rateLimitKey = req.user?.token || req.ip;
+  const rateLimitKey = req.user?.token || risuToken || req.ip;
 
   const { remaining, reset } = getStatus(rateLimitKey);
   res.set("X-RateLimit-Limit", config.modelRateLimit.toString());
@@ -92,3 +137,4 @@ export const ipLimiter = (req: Request, res: Response, next: NextFunction) => {
     next();
   }
 };
+
