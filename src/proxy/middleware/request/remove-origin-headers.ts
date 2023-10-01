41f175b7438bce2cf7b94a4404f9383e0953d9c2
Drago <contactdrago@proton.me>
2023-10-01 01:02:22 +0000

    Thanks khanon ._. https://gitgud.io/khanon/oai-reverse-proxy/-/commit/f5521aa6c372a6eb094769e47c7582bd75f8d67d

--- src/proxy/middleware/request/remove-origin-headers.ts
+++ src/proxy/middleware/request/remove-origin-headers.ts
@@ -7,4 +7,10 @@ import { ProxyRequestMiddleware } from ".";
 export const removeOriginHeaders: ProxyRequestMiddleware = (proxyReq) => {
   proxyReq.setHeader("origin", "");
   proxyReq.setHeader("referer", "");
+
+  proxyReq.removeHeader("cf-connecting-ip");
+  proxyReq.removeHeader("forwarded");
+  proxyReq.removeHeader("true-client-ip");
+  proxyReq.removeHeader("x-forwarded-for");
+  proxyReq.removeHeader("x-real-ip");
 };
