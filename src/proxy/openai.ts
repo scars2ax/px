17fd681a9689d5af812ff3b327de87b45a9853a0
Drago <contactdrago@proton.me>
2023-11-06 20:21:28 +0000

    Gpt 4 turbo 128k ! model added

--- src/proxy/openai.ts
+++ src/proxy/openai.ts
@@ -40,6 +40,7 @@ function getModelsResponse() {
     "gpt-4-32k",
     "gpt-4-32k-0613",
     "gpt-4-32k-0314", // EOL 2023-09-13
+    "gpt-4-1106-preview",
     "gpt-3.5-turbo",
     "gpt-3.5-turbo-0301", // EOL 2023-09-13
     "gpt-3.5-turbo-0613",
