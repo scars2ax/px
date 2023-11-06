5deff66091b0eede3086798e1ea8a9318ea53f06
Drago <contactdrago@proton.me>
2023-11-06 21:03:47 +0000

    Add gpt-4-vision-preview cause why not.

--- src/proxy/middleware/request/check-context-size.ts
+++ src/proxy/middleware/request/check-context-size.ts
@@ -71,6 +71,8 @@ function validateContextSize(req: Request) {
     modelMax = 16384;
   } else if (model.match(/gpt-3.5-turbo/)) {
     modelMax = 4096;
+  } else if (model.match(/gpt-4-vision-preview	/)) {
+    modelMax = 131072;
   } else if (model.match(/gpt-4-1106-preview/)) {
     modelMax = 131072;
   } else if (model.match(/gpt-4-32k/)) {
